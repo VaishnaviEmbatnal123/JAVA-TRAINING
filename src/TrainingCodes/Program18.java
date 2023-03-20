6d0517f32a899cd0a010589e12ebdb38dbbd82c9
ADC <ADC@DESKTOP-SOR2575>
2023-03-20 23:25:49 +0530

    added

---
+++ src/TrainingCodes/Program18.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package TrainingCodes;
+
+/**
+ * @author ADC
+ *
+ */
+import java.util.Scanner;
+public class Program18 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n:");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				if(i==1 || i==n||j==1||j==n)
+				{
+					System.out.print("$");
+				}
+				else
+				{
+					System.out.print("- ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
